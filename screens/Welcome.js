2b75d162e2357563504994cd790b3b130ddf320c
Adrian N. Hansa <adrian.hansa@yahoo.com>
2021-08-15 12:33:57 +0100

    Created Login screen with formik and yup for validation

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-const Welcome = () => {
+const Welcome = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome!</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+        <View style={styles.loginWrapper}>
+          <Text style={styles.loginText}>Login</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -12,6 +17,8 @@ const Welcome = () => {
 const styles = StyleSheet.create({
   container: { flex: 1, alignItems: "center", justifyContent: "center" },
   title: { color: "crimson", fontSize: 40, textAlign: "center" },
+  loginWrapper: { width: "80%", padding: 20 },
+  loginText: { fontSize: 24 },
 });
 
 export default Welcome;
--- screens/users/Login.js
+++ screens/users/Login.js
@@ -1,10 +1,66 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { Formik } from "formik";
+import * as yup from "yup";
 
-const Login = () => {
+const validationSchema = yup.object({
+  email: yup.string().required(),
+  password: yup.string().required().min(6),
+});
+
+const Login = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+          navigation.navigate("Welcome");
+        }}
+        validationSchema={validationSchema}
+      >
+        {(props) => {
+          return (
+            <>
+              <TextInput
+                value={props.values.email}
+                placeholder="Email"
+                onChangeText={props.handleChange("email")}
+                onBlur={props.handleBlur("email")}
+                style={styles.inputText}
+              />
+              <Text style={styles.error}>
+                {props.touched.email && props.errors.email}
+              </Text>
+              <TextInput
+                value={props.values.email}
+                placeholder="Email"
+                onChangeText={props.handleChange("password")}
+                onBlur={props.handleBlur("password")}
+                secureTextEntry={true}
+                style={styles.inputText}
+              />
+              <Text style={styles.error}>
+                {props.touched.password && props.errors.password}
+              </Text>
+              <TouchableOpacity
+                style={styles.buttonWrapper}
+                onPress={props.handleSubmit}
+              >
+                <Text style={styles.login}>Login</Text>
+              </TouchableOpacity>
+            </>
+          );
+        }}
+      </Formik>
     </View>
   );
 };
@@ -12,6 +68,31 @@ const Login = () => {
 const styles = StyleSheet.create({
   container: { flex: 1, alignItems: "center", justifyContent: "center" },
   title: { color: "crimson", fontSize: 40, textAlign: "center" },
+  inputText: {
+    borderColor: "grey",
+    borderRadius: 15,
+    borderWidth: 1,
+    width: "80%",
+    padding: 20,
+    marginVertical: 5,
+  },
+  buttonWrapper: {
+    width: "80%",
+    marginTop: 12,
+    height: 70,
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "grey",
+    borderRadius: 15,
+    borderWidth: 1,
+    backgroundColor: "crimson",
+  },
+  login: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "700",
+  },
+  error: { color: "crimson" },
 });
 
 export default Login;
