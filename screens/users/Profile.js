6ab453c0b68154cb9b192e16111c52575cd37ade
Adrian N. Hansa <adrian.hansa@yahoo.com>
2021-08-18 21:26:02 +0100

    Discpatched add user posts in Profile screen

--- screens/users/Profile.js
+++ screens/users/Profile.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, FlatList } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import jwtDecode from "jwt-decode";
 import { logout } from "../../redux/actions/userActions";
+import { getMyPosts } from "../../redux/actions/postActions";
 
 const Profile = ({ navigation }) => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.auth);
+  const { loading, posts, error } = useSelector((state) => state.myPosts);
   const [fullName, setFullName] = useState("");
   const getData = async () => await AsyncStorage.getItem("token");
 
   useEffect(() => {
+    dispatch(getMyPosts());
     getData()
       .then((result) => {
         setFullName(jwtDecode(result).fullName);
@@ -22,7 +25,20 @@ const Profile = ({ navigation }) => {
   return (
     <View style={styles.container}>
       {user.isAuth ? (
-        <Text style={styles.title}>Welcome, {fullName}</Text>
+        <>
+          <Text style={styles.title}>Welcome, {fullName}</Text>
+          {loading ? (
+            <Text>Loading...</Text>
+          ) : posts ? (
+            <FlatList
+              data={posts}
+              renderItem={({ item }) => <Text>{item.title}</Text>}
+              keyExtractor={(item) => item._id}
+            />
+          ) : (
+            <Text>{error}</Text>
+          )}
+        </>
       ) : (
         <Text>Hello</Text>
       )}
