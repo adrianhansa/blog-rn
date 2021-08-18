82ea15cf545fdad5170741aa5f4b8b83c34ee71d
Adrian N. Hansa <adrian.hansa@yahoo.com>
2021-08-18 19:49:48 +0100

    Created getAllPosts and getMyPosts reducers; updated the central store.

---
+++ redux/reducers/postReducers.js
@@ -0,0 +1,49 @@
+import {
+  GET_ALL_POSTS_FAIL,
+  GET_ALL_POSTS_REQUEST,
+  GET_ALL_POSTS_SUCCESS,
+  GET_MY_POSTS_FAIL,
+  GET_MY_POSTS_REQUEST,
+} from "../constants/postConstants";
+
+const initialState = {
+  auth,
+  allPosts: [],
+  myPosts: [],
+};
+
+export const getAllPostsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ALL_POSTS_REQUEST:
+      return { ...state, allPosts: { loading: true } };
+    case GET_ALL_POSTS_SUCCESS:
+      return {
+        ...state,
+        allPosts: { loading: false, allPosts: action.payload, success: true },
+      };
+    case GET_ALL_POSTS_FAIL:
+      return {
+        ...state,
+        allPosts: { loading: false, error: action.payload, success: false },
+      };
+    default:
+      return state;
+  }
+};
+
+export const getMyPostsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_MY_POSTS_REQUEST:
+      return { ...state, myPosts: { loading: true } };
+    case GET_MY_POSTS_SUCCESS:
+      return {
+        ...state,
+        myPosts: { posts: action.payload, loading: false, success: true },
+      };
+    case GET_MY_POSTS_FAIL:
+      return {
+        ...state,
+        myPosts: { loading: false, success: false, error: action.payload },
+      };
+  }
+};
--- redux/store.js
+++ redux/store.js
@@ -3,11 +3,14 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import { authReducer } from "./reducers/userReducers";
+import { getAllPostsReducer, getMyPostsReducer } from "./reducers/postReducers";
 
 const middleware = composeWithDevTools(applyMiddleware(thunk));
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  allPosts: getAllPostsReducer,
+  myPosts: getMyPostsReducer,
 });
 
 const store = createStore(rootReducer, middleware);
